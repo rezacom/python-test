746c09af01f00a27454448f055c2d80560e18a0a
rezacom <reza1880z2@gmail.com>
2021-08-13 12:50:53 +0430

    fix totalString Practice

--- 3/totalString.py
+++ 3/totalString.py
@@ -7,7 +7,24 @@ string = string.split('+')
 newStr = []
 
 for a in string:
-    newStr.append(a)
+    newStr.append(int(a))
+    # print(int(a))
+
+for i in range(0, len(newStr)):    
+    for j in range(i+1, len(newStr)):    
+        if(newStr[i] > newStr[j]):    
+            temp = newStr[i];    
+            newStr[i] = newStr[j];    
+            newStr[j] = temp;    
+
+result=[]
+
+for i in range(len(newStr)
+):
+    result.append(str(newStr[i]))
+    if(i != len(newStr) -1):
+        result.append("+")
+        print(i , len(newStr) -1)
+print(''.join(result))
 
 
-print(newStr.sort())
